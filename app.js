12f9de667761e8f9f8bdd9e31cac457b30626d93
kevinferri <kevinferri@gmail.com>
2014-10-24 20:34:28 -0400

    Set up mongoose, passport, flash, nunjucks, and other app settings

---
+++ app.js
@@ -0,0 +1,84 @@
+var express      = require('express');
+var path         = require('path');
+var favicon      = require('static-favicon');
+var logger       = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser   = require('body-parser');
+var nunjucks     = require('nunjucks');
+var mongo        = require('mongodb');
+var mongoose     = require('mongoose');
+var routes       = require('./routes/index');
+var passport     = require('passport');
+var flash        = require('connect-flash');
+var morgan       = require('morgan');
+var session      = require('express-session');
+
+var configDB = require('./config/database.js');
+
+mongoose.connect(configDB.url);
+
+var app = express();
+
+// require('./config/passport')(passport); // pass passport for configuration
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'nunjucks');
+
+nunjucks.configure('views', {
+  autoescape: true,
+  express : app,
+  watch : true
+});
+
+app.use(favicon());
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(cookieParser());
+app.use(morgan('dev')); // log every request to the console
+app.use(express.static(path.join(__dirname, 'public')));
+
+// required for passport
+app.use(session({ secret: 'ilovecookies' })); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
+
+// routes ======================================================================
+require('./routes/index')(app, passport); // load our routes and pass in our app and fully configured passport
+//app.use('/', routes);
+
+/// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+/// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error.html', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+
+module.exports = app;
