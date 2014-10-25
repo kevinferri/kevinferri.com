a792fcb67e83fc08c37b6f26bc8f649a46ba6295
kevinferri <kevinferri@gmail.com>
2014-10-24 20:37:37 -0400

    Set up basic routes for user actions

---
+++ routes/index.js
@@ -0,0 +1,54 @@
+// routes/index.js
+module.exports = function(app, passport) {
+
+  // GET homepage
+  app.get('/', function(req, res) {
+    res.render('./statics/home.html', {
+      title: 'Homepage'
+    });
+  });
+
+  // GET login form
+  app.get('./users/login', function(req, res) {
+    res.render('login.html', { 
+      title: 'Login',
+      message: req.flash('loginMessage') 
+    }); 
+  });
+
+  // process the login form
+  // app.post('/login', do all our passport stuff here);
+
+  // GET signup form
+  app.get('./users/signup', function(req, res) {
+    res.render('signup.html', { 
+      title: 'Sign up',
+      message: req.flash('signupMessage') 
+    });
+  });
+
+  // process the signup form
+  // app.post('/signup', do all our passport stuff here);
+
+  // GET profile page
+  app.get('/.users/profile', isLoggedIn, function(req, res) {
+    res.render('profile.html', {
+      title: 'My profile',
+      user : req.user // get the user out of session and pass to template
+    });
+  });
+
+  // Logs you out, takes back to homepage
+  app.get('./users/logout', function(req, res) {
+    req.logout();
+    res.redirect('/');
+  });
+};
+
+// route middleware to make sure a user is logged in
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated()) { // if user is authenticated in the session, carry on 
+    return next();
+  }
+  res.redirect('/');
+}
