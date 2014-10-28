fd5a0d4b80929f1cfd9e645d4fc1fcecec6a046e
kevinferri <kevinferri@gmail.com>
2014-10-28 12:08:19 -0400

    Update login strategy

--- config/passport.js
+++ config/passport.js
@@ -1,14 +1,11 @@
 var LocalStrategy = require('passport-local').Strategy;
 var User          = require('../models/User.js');
 
-// expose this function to our app using module.exports
 module.exports = function(passport) {
-  // used to serialize the user for the session
   passport.serializeUser(function(user, done) {
     done(null, user.id);
   });
-
-  // used to deserialize the user
+  
   passport.deserializeUser(function(id, done) {
     User.findById(id, function(err, user) {
       done(err, user);
@@ -18,14 +15,14 @@ module.exports = function(passport) {
 
   // SIGNUP STRATEGY
   passport.use('local-signup', new LocalStrategy({
-    usernameField : 'username',
-    passwordField : 'password',
+    usernameField: 'username',
+    passwordField: 'password',
     passReqToCallback : true // pass back the entire request to the callback
   },
   function(req, username, password, done) {
     // User.findOne wont fire unless data is sent back
     process.nextTick(function() {
-      User.findOne({ 'local.username' :  username }, function(err, user) {
+      User.findOne({ 'local.username':  username }, function(err, user) {
         if (err) {
           return done(err);
         }
@@ -33,6 +30,7 @@ module.exports = function(passport) {
           return done(null, false, req.flash('signupMessage', 'That username is already taken.'));
         } else {
           var newUser = new User();
+          newUser.admin = false;
           newUser.local.username = username;
           newUser.local.password = newUser.generateHash(password);
           newUser.save(function(err) {
