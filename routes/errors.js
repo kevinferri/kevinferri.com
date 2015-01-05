d9fffab406a45b872607d4b63fbf15cb201f1876
kevinferri <kevinferri@gmail.com>
2015-01-04 22:15:07 -0500

    One error page

--- routes/errors.js
+++ routes/errors.js
@@ -1,14 +1,9 @@
 module.exports = function(app, passport) {
 
-  app.get('/errors/not-logged-in', function(req, res) {
-    res.render('/errors/not-logged-in.html', {
-      title: 'Must Be Logged In',
-    });
-  });
-
-  app.get('/errors/not-authorized', function(req, res) {
-    res.render('/errors/not-authorized.html', {
-      title: 'Not Authorized',
+  app.get('/error', function(req, res) {
+    res.render('/statics/error.html', {
+      title: 'Error',
+      message: 'Error'
     });
   });
 
