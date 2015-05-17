7ba89a2b958f13ab4d52fcd00a5c8d64c9c77930
kevinferri <kevinferri@gmail.com>
2015-05-16 23:43:08 -0400

    Remove unnecessary things in gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,2 @@
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
-
-// Uglifies/minifies js
-gulp.task('minify-js', function() {
-  gulp.src('./public/javascripts/*.js')
-  .pipe(uglify())
-  .pipe(gulp.dest('build/js'));
-});
-
-// Watches js
-gulp.task('watch', function() {
-  gulp.watch('./public/javascripts/*.js', ['scripts']);
-});
-
-gulp.task('default', ['minify-js', 'watch']);
