e9586bf925b0528e7b641706c0ab83afb25ab45b
Kevin Ferri <kevinferri@gmail.com>
2015-06-26 16:43:48 -0400

    Add project model

---
+++ models/Project.js
@@ -0,0 +1,21 @@
+var mongoose = require('mongoose');
+
+var projectSchema = mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('Project', projectSchema);
